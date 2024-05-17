f812534c576cd5b8ac054f289172f5f0b171ddff
Tealler <73073943+Tealler@users.noreply.github.com>
2024-05-17 10:13:02 +0200

    First commit

---
+++ src/main/java/com/mspr_kawa/orders/main/MainApplication.java
@@ -0,0 +1,13 @@
+package com.mspr_kawa.orders.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
